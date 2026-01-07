ccc407e4c84cffb0f0a3dffe25ffbfd340e1a380
Hareesh Haridas <hareeshharidas2003@gmailcom>
2026-01-07 19:52:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const searchBox=document.querySelector('.searchBox');
+const searchBtn=document.querySelector('.searchBtn');
+const recipeContainer=document.querySelector('.recipe-container');
+
+const fetchRecipes=async(query)=>{
+  recipeContainer.innerHTML="<h2>Fetching Recipes...<h2>";
+  const data=await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+  const response=await data.json();
+  recipeContainer.innerHTML="";
+  response.meals.forEach(meal => {
+    const recipeDiv=document.createElement("div");
+    recipeDiv.classList.add('recipe');
+    recipeDiv.innerHTML=`<img src="${meal.strMealThumb}">
+    <h3>${meal.strMeal}</h3>
+    <p>${meal.strArea}</p>
+    <p>${meal.strCategory}</p>`
+    
+    recipeContainer.appendChild(recipeDiv);
+  });
+
+}
+
+searchBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const searchInput=searchBox.value.trim();
+    fetchRecipes(searchInput);
+})
